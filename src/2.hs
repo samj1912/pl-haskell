b0ed13c3ae03dec516814a37cfef43a875b9416b
manchandagaurav <manchandagaurav08@gmail.com>
2017-11-30 11:34:11 +0530

    Add proper formatting to output

--- src/2.hs
+++ src/2.hs
@@ -1,16 +1,24 @@
-import Data.Map (Map)
+-- :set -XDeriveGeneric
+-- :set -XDeriveDataTypeable
+
+import qualified GHC.Generics as G
+import Data.Data
 import qualified Data.Map as Map
-import Data.List (intercalate)
 import qualified Text.Numeral.Language.ENG as EN
 import Text.Numeral.Grammar
+import qualified Text.PrettyPrint.Tabulate as T
 -- food = [(1,"Burger",5,10), (2,"Pizza",10,20)]
 -- code, name, quantity, rate
 -- we input code and quantity
 
 ----just return false if code with a does not exist in b
 
-name code inventory = [y | (x,y,z,w) <- inventory, x==code]
-price code inventory = [w | (x,y,z,w) <- inventory, x==code]
+
+data Bought = Bought Int Int String Double Int Double deriving (Data, G.Generic)
+instance T.Tabulate Bought
+
+getname code inventory = [y | (x,y,z,w) <- inventory, x==code]
+getprice code inventory = [w | (x,y,z,w) <- inventory, x==code]
 
 notFound a b = null [(x,y,z,w)| (x,y,z,w) <- b, x == a]
 
@@ -19,7 +27,6 @@ looku code quantity tuples = [(x,y,z,w) | (x,y,z,w) <- tuples, x == code, z >= q
 update quantity key [] = []
 update quantity key ((x,y,z,w):tail) | x == key, z >= quantity = (x,y,z-quantity,w):tail
                                         | otherwise = (x,y,z,w):update quantity key tail
-
 
 queryWrongCode code quantity inventory bought totalItems= 
     do
@@ -30,68 +37,73 @@ queryWrongCode code quantity inventory bought totalItems=
 
 queryNotAvailable code quantity inventory bought totalItems = 
     do
+        putStrLn (replicate 25 '-')
         putStrLn "NOT AVAILABLE"
+        putStrLn (replicate 25 '-')
         putStrLn "More items? (0/1)"
         n <- readLn :: IO Int
         checkInventory n inventory bought totalItems
 
 
-queryAvailable :: Int -> Int -> [(Int,String,Int,Double)] -> [(Int,Int,String,Double, Int,Double)] -> Int -> IO()
+-- queryAvailable :: Int -> Int -> [(Int,String,Int,Double)] -> [Bought] -> Int -> IO()
 queryAvailable code quantity inventory bought totalItems= 
     do 
-        let nameItem = head (name code inventory)
+        let nameItem = head (getname code inventory)
+        putStrLn (replicate 25 '-')
         putStr nameItem 
-        putStrLn " AVAILABLE"
+        putStrLn "        AVAILABLE        "
+        putStrLn (replicate 25 '-')
         let newInventory = update quantity code inventory
-        let priceVal = head (price code inventory)
-        let newbought = bought ++ [(totalItems,code,nameItem, priceVal, quantity,  priceVal*fromIntegral(quantity) )]
+        let priceVal = head (getprice code inventory)
+        let value = priceVal*fromIntegral(quantity)
+        let newbought = bought ++ [Bought totalItems code nameItem priceVal quantity value]
         let newItems = totalItems +1
         putStrLn "More items? (0/1)"
         n <- readLn :: IO Int
         checkInventory n newInventory newbought newItems
 
 query code quantity inventory bought totalItems
-    | (notFound code inventory) = queryWrongCode code quantity inventory bought totalItems
-    | (null (looku code quantity inventory)) = queryNotAvailable code quantity inventory bought totalItems
-    | otherwise = queryAvailable code quantity inventory bought totalItems
+        | (notFound code inventory) = queryWrongCode code quantity inventory bought totalItems
+        | (null (looku code quantity inventory)) = queryNotAvailable code quantity inventory bought totalItems
+        | otherwise = queryAvailable code quantity inventory bought totalItems
  
 
-
 getSum [] = 0.0
-getSum ((_,_,_,_,_,value): tail) = value + getSum tail
+getSum (Bought _ _ _ _ _ value  : tail) = value + getSum tail
 
--- printList :: [(Int,Int,String,Double,Int,Double)] -> String
-printList [] = ""
-printList ((x,y,z,w,a,b):tail) = (show x) ++ " | " ++ (show y) ++ " | " ++ z ++ " | " ++ (show w) ++ " | " ++ (show a) ++ " | " ++ show(b)++ "\n" ++ printList tail
-
-
-checkInventory :: Int -> [(Int, String, Int, Double)] -> [(Int,Int,String,Double,Int,Double)] -> Int -> IO()
+-- checkInventory :: Int -> [(Int, String, Int, Double)] -> [Bought] -> Int -> IO()
 checkInventory 0 newfood bought totalItems= 
     do
-      let sum = getSum bought
-      putStrLn "---ALCHERINGA 2018, STALL 14: TANGO FAST FOOD CENTER---\n"
-      putStrLn (printList bought)
-      putStrLn (replicate 55 '-')
-      putStr "Total     " 
-      putStr (replicate 30 '*')
-      putStr "      "
-      putStrLn (show sum)
-      let total = round sum :: Int
-      putStr "     Rupees "
-      putStrLn (read (maybe " " show (EN.us_cardinal defaultInflection total)))
-      putStrLn "THANK YOU ** HAVE A NICE DAY ** PLEASE VISIT OUR STALL AGAIN"
+        let sum = getSum bought
+        putStrLn (replicate 55 '-')
+        putStrLn "   ALCHERINGA 2018, STALL 14: TANGO FAST FOOD CENTER   \n"
+        T.ppTable bought
+        putStrLn (replicate 55 '-')
+        putStr "Total     " 
+        putStr (replicate 30 '*')
+        putStr "      "
+        putStrLn (show sum)
+        putStrLn (replicate 55 '-')
+        let total = round sum :: Int
+        putStr "     Rupees "
+        putStrLn (read (maybe " " show (EN.us_cardinal defaultInflection total)))
+        putStrLn (replicate 55 '-')
+        putStrLn "THANK YOU ** HAVE A NICE DAY ** PLEASE VISIT OUR STALL AGAIN"
+        putStrLn (replicate 55 '-')
 
 checkInventory 1 inventory bought totalItems=
- do
-  putStrLn "Enter code: "
-  code <- readLn :: IO Int
-  putStrLn "Enter quantity"
-  quantity <- readLn :: IO Int
-  query code quantity inventory bought totalItems
+    do
+        putStrLn "Enter code: "
+        code <- readLn :: IO Int
+        putStrLn "Enter quantity"
+        quantity <- readLn :: IO Int
+        query code quantity inventory bought totalItems
 
 checkInventory _ inventory bought totalItems =
     do
+        putStrLn (replicate 55 '-')
         putStrLn "Please enter only (0/1)"
+        putStrLn (replicate 55 '-')
         putStrLn "More items? (0/1)"
         n <- readLn :: IO Int
         checkInventory n inventory bought totalItems
@@ -99,7 +111,7 @@ checkInventory _ inventory bought totalItems =
 main = 
     do
         let totalItems = 1 :: Int
-        let inventory = [(1,"Burger",5,10.0), (2,"Pizza",10,20.0)]
+        let inventory = [(23,"Burger",5,10.0), (40,"Pizza",10,20.0)]
         checkInventory 1 inventory [] totalItems
 
 
